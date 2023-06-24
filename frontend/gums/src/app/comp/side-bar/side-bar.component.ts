d3e9f03e37dcba0a949603228dc87c21701b1940
Norhan <norhan.mohamed@gucuae.com>
2023-06-24 13:48:09 +0300

    create, delete,update,get for fuctions with its's report

---
+++ frontend/gums/src/app/comp/side-bar/side-bar.component.ts
@@ -0,0 +1,33 @@
+import { Component, ElementRef, Renderer2 } from '@angular/core';
+
+@Component({
+  selector: 'app-side-bar',
+  templateUrl: './side-bar.component.html',
+  styleUrls: ['./side-bar.component.css']
+})
+export class SideBarComponent {
+  constructor(private renderer: Renderer2, private el: ElementRef){}
+  ngAfterViewInit() {
+    const list:NodeListOf<HTMLElement> = this.el.nativeElement.querySelectorAll('.navigation li');
+
+    list.forEach((item: HTMLElement) => {
+      if (item) {
+        this.renderer.listen(item, 'mouseover', () => {
+          list.forEach((li) => li.classList.remove('hovered'));
+          item.classList.add('hovered');
+        });
+      }
+    });
+
+    const toggle = this.el.nativeElement.querySelector('.toggle');
+    const navigation = this.el.nativeElement.querySelector('.navigation');
+    const main = this.el.nativeElement.querySelector('.main');
+
+    if (toggle) {
+      this.renderer.listen(toggle, 'click', () => {
+        navigation.classList.toggle('active');
+        main.classList.toggle('active');
+      });
+    }
+  }
+}
